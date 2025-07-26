a2e81696d687c03fc8a0be05cd8d58e310e03117
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-26 16:37:08 -0400

    adding disk usage file

---
+++ 1-diskUsage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=1 # In actual projects, this should be set to a percentage like 80 for 80%
+
+while IFS= read line 
+do
+  USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+  PARTITION=$(echo $line | awk '{print $7F}')
+  echo "Partition: $PARTITION : Usage: $USAGE%"
+
+
+done <<< $DISK_USAGE
