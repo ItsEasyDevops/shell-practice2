db6475ad8f66516d74821e9f3a803df218ed4bfc
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-26 17:42:23 -0400

    updating 1-diskUsage.sh file

--- 1-diskUsage.sh
+++ 1-diskUsage.sh
@@ -2,12 +2,17 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # In actual projects, this should be set to a percentage like 80 for 80%
+MSG=""
+
 
 while IFS= read line 
 do
   USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
   PARTITION=$(echo $line | awk '{print $7F}')
-  echo "$PARTITION :             $USAGE%"
-
+  echo "$PARTITION : $USAGE%"
+  if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="Disk usage on $PARTITION is high: $USAGE%\n"
+    fi
 
 done <<< $DISK_USAGE
