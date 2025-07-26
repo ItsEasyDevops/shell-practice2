abf228fd5f8556625bcb620b55b19020eefcd086
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-26 19:09:45 -0400

    updating 1-diskUsage.sh file

--- 1-diskUsage.sh
+++ 1-diskUsage.sh
@@ -3,6 +3,7 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 # In actual projects, this should be set to a percentage like 80 for 80%
 MSG=""
+IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 
 while IFS= read line 
@@ -16,3 +17,5 @@ do
     fi
 
 done <<< $DISK_USAGE
+
+sh mail.sh "DevOps Team" "Disk Usage Alert" "$IP" "$MSG" "mahenderkern@gmail.com" "Disk Usage Alert on $IP"
