7e7f08a48966c51ed3d8e805ce32382d831f538f
naveen21kumar95 <naveen21kumar95@gmail.com>
2025-07-26 17:39:22 -0400

    updating 1-diskUsage.sh file

--- 1-diskUsage.sh
+++ 1-diskUsage.sh
@@ -7,7 +7,7 @@ while IFS= read line
 do
   USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
   PARTITION=$(echo $line | awk '{print $7F}')
-  echo "Partition: $PARTITION : Usage: $USAGE%"
+  echo "$PARTITION :             $USAGE%"
 
 
 done <<< $DISK_USAGE
